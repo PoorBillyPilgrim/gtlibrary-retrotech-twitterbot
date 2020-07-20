a5b6e4480ff235d9264fed9184b7fb7dbfd7a771
Tyler Jones <tjjones93@gmail.com>
2020-07-20 17:26:19 -0400

    feat: connect with dummy twitter account, tweet status update

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+require('dotenv').config();
 const axios = require('axios');
 const fs = require('fs');
+const Twit = require('twit');
 const { delayReject, delayThen, delayCatch } = require('delay.ts');
+
+const data = require('./data.json');
 
 /** App originally could not connect with history.library.gatech.edu 
  *  because intermediate certificates were not bundled in the certificate chain
@@ -15,10 +19,32 @@ rootCas
 
 require('https').globalAgent.options.ca = rootCas;
 
-// Grabbed this func from https://futurestud.io/tutorials/download-files-images-with-axios-in-node-js
-async function downloadImage(url, pathToImg) {
 
-    const writeStream = fs.createWriteStream(pathToImg)
+
+const T = new Twit({
+    consumer_key: process.env.API_KEY,
+    consumer_secret: process.env.API_SECRET_KEY,
+    access_token: process.env.ACCESS_TOKEN,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET
+});
+
+T.post('statuses/update', { status: 'Look, I am tweeting again!' }, function (err, data, response) {
+    console.log(data)
+});
+
+
+
+
+const delay = () => {
+    return new Promise(resolve => {
+        setTimeout(resolve, 3000);
+    });
+}
+
+// Grabbed this func from https://futurestud.io/tutorials/download-files-images-with-axios-in-node-js
+async function downloadImage(url, imgPath) {
+
+    const writeStream = fs.createWriteStream(imgPath)
 
     const response = await axios({
         url,
@@ -37,16 +63,46 @@ async function downloadImage(url, pathToImg) {
 }
 
 const url = 'https://history.library.gatech.edu/files/original/8da0b7a926427a61f99a5aa5ec79f779.jpg'
-const img = './images/image.png'
+const imgPath = './images/image.png'
 
-
-// Using this chain has successfully showed that the image does download,
-// and then it is deleted 3 seconds later
-downloadImage(url, img).then(delayThen(3000)).then(() =>
-    fs.unlink(img, (err) => {
-        if (err) {
-            console.error(err);
-        }
-        console.log('image deleted!')
+// Need to create an async delete func
+function deleteImage(img, text) {
+    return new Promise((resolve, reject) => {
+        fs.unlink(img, (err) => {
+            if (err) {
+                reject(err)
+            }
+            resolve(`${text}, and it has been deleted!`)
+        });
     })
-);
+}
+/*
+for (let i = 0; i < data.tweets.length; i++) {
+    downloadImage(data.tweets[i].url, imgPath).then(() =>
+        fs.unlink(imgPath, (err) => {
+            if (err) {
+                console.error(err);
+            }
+            console.log(`${data.tweets[i].text}, and it has been deleted!`)
+        })
+    ).catch(err => console.error(err));
+}
+downloadImage(data.tweets[0].url, imgPath)
+    .then(deleteImage(imgPath, data.tweets[0].text))
+    .catch(err => console.error(err))
+    */
+
+/*
+for (let i = 0; i < data.tweets.length; i++) {
+    // Using this chain has successfully showed that the image does download,
+    // and then it is deleted 3 seconds later
+    downloadImage(data.tweets[i].img, img).then(delayThen(3000)).then(() =>
+        fs.unlink(img, (err) => {
+            if (err) {
+                console.error(err);
+            }
+            console.log(`${data.tweets[i].text}, and it has been deleted!`)
+        })
+    ).catch(err => console.error(err));
+}
+*/
