293c4a952af6de763719c42040eee23826b87bd2
Tyler Jones <tjjones93@gmail.com>
2020-07-24 15:05:44 -0400

    refactor: error handling

--- index.js
+++ index.js
@@ -26,50 +26,41 @@ const T = new Twit({
 });
 
 const imgPath = './images/image.png'
-
-// In progress 
-/*
-const getURL = (fileName, callback) => {
-    fs.readFile('data.json', (err, data) => {
-        let arr = JSON.parse(data);
-        let url = arr.tweets[0].url;
-        if (err) {
-            console.error(err)
-        }
-        callback(url);
-    })
+const handleError = (err) => {
+    if (err) {
+        console.error(err);
+    }
 }
-*/
-
 
 // Download image from url
 const downloadImage = (url, path, callback) => {
-    console.log('downloading image')
+    console.log('downloading image');
     request.head(url, () => {
         request(url)
+            .on('error', (err) => console.error(err))
             .pipe(fs.createWriteStream(path))
             .on('close', callback)
-    })
+    });
 }
 
 // Read ./images/image.png and upload to Twitter
 const postTweet = (img, callback) => {
     fs.readFile(img, { encoding: 'base64' }, (err, b64content) => {
+        handleError(err);
         console.log('uploading image');
         T.post('media/upload', { media_data: b64content }, (err, img, response) => {
+            handleError(err);
             if (!err) {
                 fs.readFile('data.json', (err, data) => {
-                    let arr = JSON.parse(data);
-                    let text = JSON.stringify(arr.tweets[0].text);
-                    let url = JSON.stringify(arr.tweets[0].url)
+                    handleError(err);
+                    let arr = JSON.parse(data),
+                        text = JSON.stringify(arr.tweets[0].text),
+                        url = JSON.stringify(arr.tweets[0].url);
                     // The second parameter is an object that identifies the values to be tweeted
                     // media_id == image
                     // status == text of status update
                     T.post('statuses/update', { status: `Check out ${text.slice(1, -1)} at ${url.slice(1, -1)}`, media_ids: new Array(img.media_id_string) }, (err, data, response) => {
-
-                        if (err) {
-                            console.error(err)
-                        }
+                        handleError(err);
                         console.log(data);
                         if (callback) {
                             callback(err);
@@ -82,11 +73,9 @@ const postTweet = (img, callback) => {
 }
 
 // Delete ./images/image after upload
-function deleteImage(img, text, callback) {
+const deleteImage = (img, text, callback) => {
     fs.unlink(img, (err) => {
-        if (err) {
-            console.error(err)
-        }
+        handleError(err);
         console.log(`${text}, and it has been deleted!`)
     });
 }
@@ -94,11 +83,11 @@ function deleteImage(img, text, callback) {
 // Read data.json file, deletes the first entry, and saves the new file
 const editJsonFile = (fileName, callback) => {
     fs.readFile(fileName, 'utf-8', (err, data) => {
-        console.log('file has been read')
+        handleError(err);
         const arr = JSON.parse(data);
         arr.tweets.shift();
         fs.writeFile(fileName, JSON.stringify(arr), (err) => {
-            if (err) throw err;
+            handleError(err);
             console.log('file saved!')
         });
     });
@@ -108,15 +97,13 @@ const editJsonFile = (fileName, callback) => {
 // Testing: successfully tweets every 5 seconds
 const job = new CronJob('*/5 * * * * *', () => {
     fs.readFile('data.json', (err, data) => {
-        let arr = JSON.parse(data);
-        let url = JSON.stringify(arr.tweets[0].url);
+        handleError(err);
+        let arr = JSON.parse(data),
+            url = JSON.stringify(arr.tweets[0].url);
         // JSON.stringify() wraps the already quoted string in more quotes
         // slice(1, -1) removes the extra quotes
         downloadImage(url.slice(1, -1), imgPath, () => {
             postTweet(imgPath, (err) => {
-                if (err) {
-                    console.error(err);
-                }
                 deleteImage(imgPath, arr.tweets[0].text);
                 editJsonFile('data.json');
             })
